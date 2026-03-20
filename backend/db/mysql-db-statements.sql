08a4bce3341c23cf549185626e7a39a17afea59b
Shruthilaya Jindamwar <jindamwarshruthilaya@gmail.com>
2026-03-20 05:43:19 +0530

    add sql

---
+++ backend/db/mysql-db-statements.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE school;
+
+USE school;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    course VARCHAR(100)
+);
+
+INSERT INTO
+    students (name, age, course)
+VALUES
+    ('Rahul', 20, 'Computer Science'),
+    ('Anita', 22, 'Mathematics'),
+    ('Kiran', 19, 'Physics');
+    
+SELECT * FROM school.students;
